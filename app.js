c8283f488b01fa21acab8f0491dd7f51b2bb7de3
Oleg Brusnik <oleg.brusnik.93@mail.ru>
2023-09-01 16:01:33 +0500

    exercize 3 done

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const parent = document.querySelector("ul");
-
-console.log(parent);
+const parentElement = document.querySelector(".container");
 
 const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/todos");
+function addUserInfo(userInfo, HTMLTag, CSSClass) {
+  const tag = document.createElement(`${HTMLTag}`);
+  tag.classList.add(`${CSSClass}`);
+  tag.textContent = userInfo;
+  return tag;
+}
 
 xhr.onload = () => {
   if (xhr.status >= 200 && xhr.status < 300) {
     const response = JSON.parse(xhr.responseText);
-    for (let i = 0; i <= 20; i++) {
-      parent.innerHTML += `<li>${response[i].title}</li>`;
+    console.log(response);
+    for (let i = 0; i <= 9; i++) {
+      const div = document.createElement("div");
+      div.classList.add("user-info");
+      div.appendChild(addUserInfo(response[i].name, "h3", "name"));
+      div.appendChild(addUserInfo(response[i].email, "p", "email"));
+      div.appendChild(addUserInfo(response[i].body, "p", "body"));
+      parentElement.appendChild(div);
     }
   } else {
-    console.error("Ошибка при загрузке данных", xhr.status);
+    console.error("Произошла ошибка при запросе данных с сервера");
   }
 };
+
+xhr.open("GET", "https://jsonplaceholder.typicode.com/comments");
 
 xhr.send();
